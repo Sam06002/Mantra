e7252aea7de140f79d33b5abad9b9942e06b0fc0
Web.Warrior <55346881+The-lYNCAN@users.noreply.github.com>
2021-09-10 21:21:24 +0530

    Home Done

--- components/navBar.js
+++ components/navBar.js
@@ -1,10 +1,34 @@
 import s from '../styles/navbar.module.css'
 import Head from 'next/head'
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 const NavBar = () => {
+
+    useEffect(() => {
+        window.onscroll = () => {
+            const con = document.getElementsByClassName(s.con)[0]
+            if(con.offsetTop > 6){
+                con.style.background = "#007bff"
+                document.getElementsByClassName(s.item)[0].style.color = "white"
+                document.getElementsByClassName(s.item)[1].style.color = "white"
+                document.getElementsByClassName(s.item)[2].style.color = "white"
+                document.getElementsByClassName(s.item)[3].style.color = "white"
+                document.getElementsByClassName(s.item)[4].style.color = "white"
+            }else{
+                con.style.background = "white"
+                document.getElementsByClassName(s.item)[0].style.color = "black"
+                document.getElementsByClassName(s.item)[1].style.color = "black"
+                document.getElementsByClassName(s.item)[2].style.color = "black"
+                document.getElementsByClassName(s.item)[3].style.color = "black"
+                document.getElementsByClassName(s.item)[4].style.color = "black"
+                
+            }
+        }
+    }, [])
+
     return(
-        <div>
+        <div className={s.con}>
         <Head>
         {/* <!-- JavaScript Bundle with Popper --> */}
 {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossOrigin="anonymous"></script> */}
@@ -15,7 +39,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-sm">
                 <div className="container-md">
                     <a className="navbar-brand text-dark" id="test">MANTRA</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">CLICK ME BITCH!!</button>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><img width="50px" src="/spade.png" /></button>
 
                     <div className="collapse navbar-collapse" id="navmenu">
                         <ul className="navbar-nav ms-auto">
